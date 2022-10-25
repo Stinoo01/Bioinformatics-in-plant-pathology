846612d4cde39e3d638b1b8cd528863cceff17fe
lfaino <M3Fw3K2h>
2022-10-25 12:57:45 +0200

    test

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from Bio import SeqIO
 BWAI = "bwa index %s"
 BWA = "bwa mem -t %s %s %s"
 
-def download():
+def download(reference):
     Entrez.email = 'A.N.Other@example.com'
     #taking data
-    handle = Entrez.efetch(db='nucleotide', id = 'NZ_CP007265.1', rettype = 'fasta', retmode = 'text')
+    handle = Entrez.efetch(db='nucleotide', id = reference, rettype = 'fasta', retmode = 'text')
     record = SeqIO.read(handle, 'fasta')
     #test
     #taking results
@@ -26,21 +26,53 @@ def download():
     reference.close()
     return("rpova.txt")
 
-def bwa(reference):
+def bwa(reference, sraFile, pe):
     #BWAI = "bwa index %s"
     #BWA = "bwa mem -t %s %s %s"
 
     bi = BWAI % reference
-    bwa_index = sb.Popen(bi, shell=True, cwd="/tmp")
+    bwa_index = sb.Popen(bi, shell=True)
     bwa_index.communicate()
+    if pe:
+        sraFile = sraFile + "_1.fastq " + sraFile + "_2.fastq"
+    else:
+        sraFile = sraFile + ".fastq "
+    bwa_mem = BWA % ("10", reference, sraFile)
+    bwa_mem_out = sb.Popen(bwa_mem, shell=True)
+    bwa_mem_out.communicate()
+    print("done")
     # a = subprocess.getstatusoutput('mkdir index')
     # b = subprocess.getstatusoutput('mv ' + reference + ' index/')
     # c = subprocess.getstatusoutput('bwa index index/'+ reference)
     # d = subprocess.getstatusoutput('bwa mem -t 8 index/GCF_000013925.1_ASM1392v2_genomic.fna.gz P7741_R1.fastq.gz P7741_R2.fastq.gz > output.sam')
 
+def sra(species):
+    # samples
+    sra_numbers = [species]
+
+    # this will download the .sra files to ~/ncbi/public/sra/ (will create directory if not present)
+    for sra_id in sra_numbers:
+        #print ("Currently downloading: " + sra_id)
+        prefetch = 'prefetch ' + sra_id
+        #print ("The command used was: " + prefetch)
+        sb.call(prefetch, shell=True)
+
+    # this will extract the .sra files from above into a folder named 'fastq'
+    for sra_id in sra_numbers:
+        #print ("Generating fastq for: " + sra_id)
+        fastq_dump = "fasterq-dump " + sra_id
+        #print ("The command used was: " + fastq_dump)
+        sb.call(fastq_dump, shell=True)
+    # if ....:
+    pe = True
+    # else:
+    #     pe = False
+    return(pe, species)
+
 def main():
-    reference = download()
-    bwa(reference)
+    pe, sraFile = sra("SRR21936789")
+    reference = download("NZ_CP007265.1")
+    bwa(reference, sraFile, pe)
 
 if __name__ == '__main__':
     main()
