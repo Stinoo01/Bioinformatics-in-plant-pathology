6ac95b59837d98a9cbcda128db984904bb7eacc5
Stinoo01 <93342130+Stinoo01@users.noreply.github.com>
2022-10-25 11:21:43 +0200

    Add files via upload

---
+++ plant pathology.py
@@ -0,0 +1,20 @@
+from Bio import Entrez
+from Bio import SeqIO
+
+Entrez.email = 'A.N.Other@example.com'
+#taking data
+handle = Entrez.efetch(db='nucleotide', id = 'NZ_CP007265.1', rettype = 'fasta', retmode = 'text')
+record = SeqIO.read(handle, 'fasta')
+
+#taking results
+seq = record.seq
+seqq = str(seq)
+#length of the result
+length = len(seqq)
+str_length = str(length)
+
+#output in a file
+f= open("rpova.txt", "w")
+f.write(seqq+'\n'+str_length)
+f.close()
+
