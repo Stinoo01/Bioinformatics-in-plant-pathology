36a7e6c61b89b019fa446240e0a1a3598bcb5546
lfaino <M3Fw3K2h>
2022-11-08 12:13:48 +0100

    test

--- main.py
+++ main.py
@@ -1,78 +1,86 @@
 #! /usr/bin/python3.10
 import subprocess as sb
 from Bio import Entrez
+import os
 from Bio import SeqIO
 
 
 BWAI = "bwa index %s"
 BWA = "bwa mem -t %s %s %s"
+SMS = "samtools sort -@ %s -O BAM -o %s"
 
 def download(reference):
+    cwd = os.getcwd()
     Entrez.email = 'A.N.Other@example.com'
     #taking data
-    handle = Entrez.efetch(db='nucleotide', id = reference, rettype = 'fasta', retmode = 'text')
-    record = SeqIO.read(handle, 'fasta')
-    #test
-    #taking results
-    seq = record.seq
-    seqq = str(seq)
-    #length of the result
-    length = len(seqq)
-    str_length = str(length)
+    net_handle = Entrez.efetch(db='nucleotide', id = reference, rettype = 'fasta', retmode = 'text')
+    data = net_handle.read()
+    net_handle.close()
 
-    #output in a file
-    reference= open("/tmp/rpova.txt", "w")
-    reference.write(seqq+'\n'+str_length)
-    reference.close()
-    return("rpova.txt")
+    ref = os.path.join(cwd, "my_file.fasta")
 
-def bwa(reference, sraFile, pe):
+    out_handle = open(ref, "w")
+    out_handle.write(data)
+    out_handle.close()
+
+
+
+    return(ref)
+
+def bwa(reference, sraFile):
     #BWAI = "bwa index %s"
     #BWA = "bwa mem -t %s %s %s"
 
     bi = BWAI % reference
     bwa_index = sb.Popen(bi, shell=True)
     bwa_index.communicate()
-    if pe:
-        sraFile = sraFile + "_1.fastq " + sraFile + "_2.fastq"
-    else:
-        sraFile = sraFile + ".fastq "
-    bwa_mem = BWA % ("10", reference, sraFile)
-    bwa_mem_out = sb.Popen(bwa_mem, shell=True)
-    bwa_mem_out.communicate()
-    print("done")
+    for sra in sraFile:
+        if sra[1]:
+            sraFileName = sra[0] + "_1.fastq " + sra[0] + "_2.fastq"
+        else:
+            sraFileName = sra[0] + ".fastq "
+        bwa_mem = BWA % ("10", reference, sraFileName)
+        samtools_sort = SMS % (10, sra[0] + ".sorted.bam")
+        bwa_mem_out = sb.Popen(bwa_mem, stdout=sb.PIPE , shell=True)
+        samtools_sort_out = sb.Popen(samtools_sort, stdin=bwa_mem_out.stdout, shell=True)
+        samtools_sort_out.communicate()
+        print("done")
     # a = subprocess.getstatusoutput('mkdir index')
     # b = subprocess.getstatusoutput('mv ' + reference + ' index/')
     # c = subprocess.getstatusoutput('bwa index index/'+ reference)
     # d = subprocess.getstatusoutput('bwa mem -t 8 index/GCF_000013925.1_ASM1392v2_genomic.fna.gz P7741_R1.fastq.gz P7741_R2.fastq.gz > output.sam')
 
-def sra(species):
+def sra(sra_numbers):
+    cwd = os.getcwd()
+    pe_sra = []
     # samples
-    sra_numbers = [species]
-
+    #sra_numbers = [species]
     # this will download the .sra files to ~/ncbi/public/sra/ (will create directory if not present)
-    for sra_id in sra_numbers:
-        #print ("Currently downloading: " + sra_id)
-        prefetch = 'prefetch ' + sra_id
-        #print ("The command used was: " + prefetch)
-        sb.call(prefetch, shell=True)
+    # for sra_id in sra_numbers:
+    #     #print ("Currently downloading: " + sra_id)
+    #     prefetch = 'prefetch ' + sra_id + "-O " + cwd
+    #     #print ("The command used was: " + prefetch)
+    #     sb.call(prefetch, shell=True)
 
     # this will extract the .sra files from above into a folder named 'fastq'
     for sra_id in sra_numbers:
         #print ("Generating fastq for: " + sra_id)
-        fastq_dump = "fasterq-dump " + sra_id
+        fastq_dump = "fasterq-dump " + sra_id + " -O " + cwd
         #print ("The command used was: " + fastq_dump)
         sb.call(fastq_dump, shell=True)
-    # if ....:
-    pe = True
-    # else:
-    #     pe = False
-    return(pe, species)
+
+        if os.path.exists(os.path.join(cwd, sra_id + "_1.fastq")) :
+            pe = True
+        else:
+            pe = False
+        pe_sra.append((sra_id, pe))
+
+    return(pe_sra)
 
 def main():
-    pe, sraFile = sra("SRR21936789")
+    pe_sra = sra(["SRR21936789","SRR21936788"])
     reference = download("NZ_CP007265.1")
-    bwa(reference, sraFile, pe)
+    bwa(reference, pe_sra)
 
 if __name__ == '__main__':
     main()
